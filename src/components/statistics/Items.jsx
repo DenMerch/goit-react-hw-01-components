1ae718f9d135d49a61b3e8b444d3805b40dbaeba
DenMerch <rosignolli@gmail.com>
2023-03-05 17:24:27 +0200

    second

--- src/components/statistics/Items.jsx
+++ src/components/statistics/Items.jsx
@@ -1,11 +1,10 @@
 import css from './Items.module.css'
 export const Items = ({ data }) => {
 
-
     return data.map(item => {
-
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
         return (
-            <li className={css.items} key={item.id}>
+            <li key={item.id} style={{ backgroundColor: randomColor }}>
                 <span className="label">{item.label}</span>
                 <span className="percentage">{item.percentage}%</span>
             </li>
